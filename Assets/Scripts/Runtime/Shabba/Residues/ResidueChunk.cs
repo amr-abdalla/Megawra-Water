645008266fd04be3ce69e054f2fee10cad86e1fa
amr-abdalla <amr_alaa2@aucegypt.edu>
2023-04-15 16:45:32 +0200

    Used OverlapSphere to detect adjacent chunks instead of ResidueGrid

--- Assets/Scripts/Runtime/Shabba/Residues/ResidueChunk.cs
+++ Assets/Scripts/Runtime/Shabba/Residues/ResidueChunk.cs
@@ -12,6 +12,9 @@ public class ResidueChunk : MonoBehaviour
 	public const float _PushValueMultiplier = 0.1f;
 	public const float _PushSpeedMultiplier = 0.05f;
 	public const float _randomPushRotationRange = 150f;
+	private const float _AdjacentPushValueMultiplier = 0.5f;
+	private const float _AdjacentPushSpeedMultiplier = 0.5f;
+	private const string _ChunkLayer = "Chunk";
 
 	#region PUBLIC API
 	public IReadOnlyList<ResidueParticle> GetResidueParticles()
@@ -24,17 +27,6 @@ public class ResidueChunk : MonoBehaviour
 	{
 		residueParticles.ForEach(particle => particle.particleMovement.Init());
 	}
-
-	public void PushAllParticles(float value, Vector3 pushDirection, float speed)
-	{
-		foreach (ResidueParticle particle in residueParticles)
-		{
-			particle.particleMovement.GetPushed(value, Quaternion.AngleAxis(Random.Range(- _randomPushRotationRange, _randomPushRotationRange), Vector2.right) * pushDirection, speed);
-		}
-
-		OnGetPushed?.Invoke(this, pushDirection, value, speed);
-	}
-
 	#endregion
 
 	#region UNITY
@@ -69,11 +61,43 @@ public class ResidueChunk : MonoBehaviour
 		residueParticle.OnCollect -= RemoveResidueParticleFromCachedList;
 		residueParticles.Remove(residueParticle);
 
-		if(residueParticles.Count == 0)
+		if (residueParticles.Count == 0)
 		{
 			OnDestroy?.Invoke(this);
 			Destroy(gameObject);
 		}
+	}
+
+	private void PushAllParticles(float value, Vector3 pushDirection, float speed)
+	{
+		foreach (ResidueParticle particle in residueParticles)
+		{
+			particle.particleMovement.GetPushed(value, Quaternion.AngleAxis(Random.Range(-_randomPushRotationRange, _randomPushRotationRange), Vector2.right) * pushDirection, speed);
+		}
+
+		PushAdjacentChunks(pushDirection * _AdjacentPushValueMultiplier, value, speed * _AdjacentPushSpeedMultiplier);
+		OnGetPushed?.Invoke(this, pushDirection, value, speed);
+	}
+
+	private void PushAdjacentChunks(Vector3 direction, float pushValue, float pushSpeed)
+	{
+		var adjacentChunks = GetAdjacentChunks();
+
+		foreach (ResidueChunk chunk in adjacentChunks)
+		{
+			if (chunk.GetResidueParticles()[0].particleMovement.currentPush is null)
+			{
+				chunk.PushAllParticles(pushValue, direction, pushSpeed);
+			}
+		}
+	}
+
+	private IReadOnlyList<ResidueChunk> GetAdjacentChunks()
+	{
+		List<Collider2D> adjacentChunkColliders = Physics2D.OverlapCircleAll(transform.position, 8f, LayerMask.GetMask(_ChunkLayer)).ToList();
+		adjacentChunkColliders.Remove(GetComponent<Collider2D>());
+
+		return adjacentChunkColliders.Select(chunk => chunk.GetComponent<ResidueChunk>()).ToList();
 	}
 
 	#endregion
