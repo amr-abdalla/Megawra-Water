6778478350d8da0a9fbaaaed7656aaa02cd37951
nyuriumuri <nyuriumuri@gmail,com>
2023-05-01 19:40:53 +0300

    r to restart scene

--- Assets/Scripts/Runtime/Shabba/Movement/ShabbaInput.cs
+++ Assets/Scripts/Runtime/Shabba/Movement/ShabbaInput.cs
@@ -11,13 +11,25 @@ public class ShabbaInput : MonoBehaviour
     [SerializeField] private InputActionReference pushAction;
     [SerializeField] private InputActionReference rotateAction;
 
+    [SerializeField] private InputActionReference restartAction;
     private void InitInput()
     {
         pushAction.action.Enable();
         rotateAction.action.Enable();
         pushAction.action.performed += ctx => shabbaMoveAction.Push(7f);
         rotateAction.action.performed += ctx => shabbaMoveAction.Rotate(ctx.ReadValue<Vector2>());
+
+        restartAction.action.Enable();
+        restartAction.action.performed += ctx => RestartScene();
         //rotateAction.action.canceled += ctx => RotateVelocity(0);
+    }
+
+    private void RestartScene()
+    {
+        // get the current scene name
+        string currentSceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+        // load the current scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene(currentSceneName);
     }
 
     private void Awake()
