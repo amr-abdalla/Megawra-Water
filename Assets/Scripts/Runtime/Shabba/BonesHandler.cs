6029c00206f9c4243f43a4200feba537fb52b34e
nyuriumuri <nyuriumuri@gmail.com>
2024-03-02 14:59:30 +0200

    little tweaks to bone rotation

--- Assets/Scripts/Runtime/Shabba/BonesHandler.cs
+++ Assets/Scripts/Runtime/Shabba/BonesHandler.cs
@@ -2,40 +2,47 @@ using UnityEngine;
 
 public class BonesHandler : MonoBehaviour
 {
-	[SerializeField] Transform bone2;
-	[SerializeField] Transform bone3;
-	[SerializeField] Transform bone4;
+    [SerializeField] Transform bone2;
+    [SerializeField] Transform bone3;
+    [SerializeField] Transform bone4;
 
-	public struct bonesPositionPreset
-	{
-		public Vector2 bone2Pos;
-		public Vector2 bone3Pos;
-		public Vector2 bone4Pos;
 
-		public bonesPositionPreset(Vector2 bone2Pos, Vector2 bone3Pos, Vector2 bone4Pos)
-		{
-			this.bone2Pos = bone2Pos;
-			this.bone3Pos = bone3Pos;
-			this.bone4Pos = bone4Pos;
-		}
-	}
+    [SerializeField]
+    private AnimationCurve rotationCurve;
 
-	public void SetBones(float t)
-	{
-		float bone2y = Mathf.Lerp(4.872665e-08f, 0.01686376f, t);
-		float bone3y = Mathf.Lerp(4.624603e-08f, 0.06601496f, t);
-		float bone4y = Mathf.Lerp(3.957644e-09f, 0.09823086f, t);
+    public struct bonesPositionPreset
+    {
+        public Vector2 bone2Pos;
+        public Vector2 bone3Pos;
+        public Vector2 bone4Pos;
 
-		float bone2RotZ = Mathf.Lerp(0, 1.724f, t);
-		float bone3RotZ = Mathf.Lerp(0, 10.453f, t);
-		float bone4RotZ = Mathf.Lerp(0, 0, t);
 
-		bone2.localPosition = new(0.364f, bone2y);
-		bone3.localPosition = new(0.363f, bone3y);
-		bone4.localPosition = new(0.379f, bone4y);
-		bone2.localRotation = Quaternion.Euler(0, 0, bone2RotZ);
-		bone3.localRotation = Quaternion.Euler(0, 0, bone3RotZ);
-		bone4.localRotation = Quaternion.Euler(0, 0, bone4RotZ);
-	}
+
+        public bonesPositionPreset(Vector2 bone2Pos, Vector2 bone3Pos, Vector2 bone4Pos)
+        {
+            this.bone2Pos = bone2Pos;
+            this.bone3Pos = bone3Pos;
+            this.bone4Pos = bone4Pos;
+        }
+    }
+
+
+    public void SetBones(float t)
+    {
+        // float bone2y = Mathf.Lerp(4.872665e-08f, 0.01686376f, t);
+        // float bone3y = Mathf.Lerp(4.624603e-08f, 0.06601496f, t);
+        // float bone4y = Mathf.Lerp(3.957644e-09f, 0.09823086f, t);
+
+        float bone2RotZ = rotationCurve.Evaluate(t) * -1.724f * 1.2f;
+        float bone3RotZ = rotationCurve.Evaluate(t) * -10.453f * 1.2f;
+        float bone4RotZ = 0f;
+
+        // bone2.localPosition = new(0.364f, bone2y);
+        // bone3.localPosition = new(0.363f, bone3y);
+        // bone4.localPosition = new(0.379f, bone4y);
+        bone2.localRotation = Quaternion.Euler(0, 0, bone2RotZ);
+        bone3.localRotation = Quaternion.Euler(0, 0, bone3RotZ);
+        bone4.localRotation = Quaternion.Euler(0, 0, bone4RotZ);
+    }
 
 }
