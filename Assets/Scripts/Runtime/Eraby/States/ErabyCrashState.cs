8ed5d5dbc825ee29a3cd731b9bbbd8b058704232
3mr-TheBadNomads <amrabdalla@thebadnomads.com>
2024-03-22 19:05:06 +0200

    Added GoToMainMenu to ErabyCrashState.cs

--- Assets/Scripts/Runtime/Eraby/States/ErabyCrashState.cs
+++ Assets/Scripts/Runtime/Eraby/States/ErabyCrashState.cs
@@ -1,59 +1,56 @@
 // using Cinemachine;
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
 
 public class ErabyCrashState : ErabyAbstractLandingState
 {
-    [SerializeField]
-    private float minBounceVelocityY = 0f;
+	[SerializeField]
+	private float minBounceVelocityY = 0f;
 
- 
+	#region STATE API
+	protected override void onStateEnter()
+	{
+		Debug.Log("Enter crash");
+		dataProvider.numCrashes += 1;
+		if (dataProvider.numCrashes >= 5)
+		{
+			// TODO: Goto menu
+			UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
+		}
 
-    #region STATE API
-    protected override void onStateEnter()
-    {
-        Debug.Log("Enter crash");
-        dataProvider.numCrashes += 1;
-        if(dataProvider.numCrashes >= 5){
-          // TODO: Goto menu
-        }
+		base.onStateEnter();
+	}
 
-        
-        
- 
-        base.onStateEnter();
-    }
+	override protected IEnumerator landingSequence()
+	{
+		yield return new WaitForSeconds(landingTime);
 
-    override protected IEnumerator landingSequence()
-    {
-        yield return new WaitForSeconds(landingTime);
+		// Debug.Log("LaunchVelocit")
+		if (dataProvider.launchVelocityY > minBounceVelocityY)
+		{
+			if (tapManager.isTapped())
+				setState<ErabySuperLaunchState>();
+			else
+				setState<ErabyCrashedLaunchState>();
+		}
+		else
+			setState<ErabyIdleState>();
+	}
 
-        // Debug.Log("LaunchVelocit")
-        if (dataProvider.launchVelocityY > minBounceVelocityY)
-        {
-            if (tapManager.isTapped())
-                setState<ErabySuperLaunchState>();
-            else
-                setState<ErabyCrashedLaunchState>();
-        }
-        else
-            setState<ErabyIdleState>();
-    }
+	override protected void applyTapMulipier()
+	{
+		//Debug.LogError("Apply tap multiplier");
+		float newVelocityX = clampVelocityX(
+			-Mathf.Abs(dataProvider.landingVelocityX) * tapMultiplier
+		);
 
-    override protected void applyTapMulipier()
-    {
-        //Debug.LogError("Apply tap multiplier");
-        float newVelocityX = clampVelocityX(
-            -Mathf.Abs(dataProvider.landingVelocityX) * tapMultiplier
-        );
+		dataProvider.launchVelocityX = newVelocityX;
+		dataProvider.launchVelocityY *= tapMultiplier;
+	}
 
-        dataProvider.launchVelocityX = newVelocityX;
-        dataProvider.launchVelocityY *= tapMultiplier;
-    }
+	#endregion
 
-    #endregion
+	#region PRIVATE
 
-    #region PRIVATE
-
-    #endregion
+	#endregion
 }
