939870e80fa5012993ecdfbdaef98fbb1feb2792
karimaboulenein <karim@largelabs.fr>
2024-03-22 16:49:51 +0200

    yalahwy

--- Assets/Scripts/Runtime/Eraby/MusicManager.cs
+++ Assets/Scripts/Runtime/Eraby/MusicManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,25 +6,51 @@ using UnityEngine;
 public class MusicManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField]
-    private AudioClip[] audioclips;
+    [SerializeField] private AudioClip[] audioclips;
+    [SerializeField]  private PhysicsBody2D erabyBody;
+    [SerializeField] private MusicCrossfade crossfade;
 
-  [SerializeField]
-  private PhysicsBody2D erabyBody;
-    private float minVelocity =0f;
-    private float maxVelocity =100f;
-    void Start()
+    private float minVelocity = 0f;
+    [SerializeField] private float maxVelocity = 100f;
+
+    int selectedIndex = 0;
+    Coroutine playClipRoutine = null;
+
+
+    private void Awake()
     {
-        
+        crossfade.Play(audioclips[selectedIndex]);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        int index = getSelectedIndex();
 
+        if(selectedIndex != index)
+        {
+            selectedIndex = index;
+            this.DisposeCoroutine(ref playClipRoutine);
+            playClipRoutine = StartCoroutine(playWithDelay());
+        }
+    }
+
+    IEnumerator playWithDelay()
+    {
+        yield return new WaitForSeconds(1.5f);
+        crossfade.Play(audioclips[selectedIndex]);
+
+        this.DisposeCoroutine(ref playClipRoutine);
+    }
+
+    int getSelectedIndex()
+    {
         float velocity = Mathf.Abs(erabyBody.VelocityX);
-        Mathf.InverseLerp()
+        float t = Mathf.InverseLerp(minVelocity, maxVelocity, velocity);
 
-        
+        float indexF = Mathf.Lerp(0, audioclips.Length - 1, t);
+        int index = Mathf.RoundToInt(indexF);
+        index = Mathf.Clamp(index, 0, audioclips.Length - 1);
+
+        return index;
     }
 }
