9eb65ba23a7bbf71d5c44a9adc670b2089279765
3mr-TheBadNomads <87068315+3mr-TheBadNomads@users.noreply.github.com>
2025-04-29 02:19:20 +0300

    Shabba sprites working but not clean

--- Assets/Scripts/Runtime/Shabba/ScoreTracker.cs
+++ Assets/Scripts/Runtime/Shabba/ScoreTracker.cs
@@ -3,4 +3,6 @@ public static class ScoreTracker
 	public static float CurrentScore = 0;
 	public static float TotalScore = 0;
 	public static float MaxScore = 0;
+
+	public static float GetCurrentScorePercentage() => CurrentScore / MaxScore * 100f;
 }
--- Assets/Scripts/Runtime/Shabba/ShabbaAnimationHandler.cs
+++ Assets/Scripts/Runtime/Shabba/ShabbaAnimationHandler.cs
@@ -2,29 +2,77 @@ using UnityEngine;
 
 public class ShabbaAnimationHandler : MonoBehaviour
 {
-	[SerializeField] private SpriteFrameSwapper dashAnimation;
-	[SerializeField] private SpriteFrameSwapper idleAnimation;
+	[SerializeField] private SpriteFrameSwapper[] dashAnimations;
+	[SerializeField] private SpriteFrameSwapper[] idleAnimations;
 	[SerializeField] private BonesHandler bonesHandler;
+
+
+	private SpriteFrameSwapper GetCurrentDashAnimation()
+	{
+		float currentScorePercentage = ScoreTracker.GetCurrentScorePercentage();
+		Debug.Log(currentScorePercentage);
+
+		if (currentScorePercentage > 40f)
+		{
+			dashAnimations[0].Stop();
+			dashAnimations[1].Stop();
+			return dashAnimations[2];
+		}
+
+		if (currentScorePercentage > 20f)
+		{
+			dashAnimations[0].Stop();
+			dashAnimations[2].Stop();
+			return dashAnimations[1];
+		}
+
+		dashAnimations[1].Stop();
+		dashAnimations[2].Stop();
+		return dashAnimations[0];
+	}
+
+	private SpriteFrameSwapper GetCurrentIdleAnimation()
+	{
+		float currentScorePercentage = ScoreTracker.GetCurrentScorePercentage();
+
+		if (currentScorePercentage > 40f)
+		{
+			idleAnimations[0].Stop();
+			idleAnimations[1].Stop();
+			return idleAnimations[2];
+		}
+
+		if (currentScorePercentage > 20f)
+		{
+			idleAnimations[0].Stop();
+			idleAnimations[2].Stop();
+			return idleAnimations[1];
+		}
+
+		idleAnimations[1].Stop();
+		idleAnimations[2].Stop();
+		return idleAnimations[0];
+	}
 
 	public void InvokeDashAnimation()
 	{
-		idleAnimation.Stop();
-		dashAnimation.ResetAnimation();
-		dashAnimation.Play();
+		GetCurrentIdleAnimation().Stop();
+		GetCurrentDashAnimation().ResetAnimation();
+		GetCurrentDashAnimation().Play();
 	}
 
 	public void PlayIdleAnimation()
 	{
-		dashAnimation.Stop();
-		idleAnimation.ResetAnimation();
-		idleAnimation.Play();
+		GetCurrentDashAnimation().Stop();
+		GetCurrentIdleAnimation().ResetAnimation();
+		GetCurrentIdleAnimation().Play();
 	}
 
 	public void ResetAnimation()
 	{
-		idleAnimation.Stop();
-		Sprite firstSprite = idleAnimation.GetFirstFrame();
-		idleAnimation.ResetAnimation(firstSprite);
+		GetCurrentIdleAnimation().Stop();
+		Sprite firstSprite = GetCurrentIdleAnimation().GetFirstFrame();
+		GetCurrentIdleAnimation().ResetAnimation(firstSprite);
 	}
 
 	public void OnRotate(float rotationDiff)
@@ -37,13 +85,16 @@ public class ShabbaAnimationHandler : MonoBehaviour
 
 	private void OnFinishDashAnimation()
 	{
-		dashAnimation.Stop();
-		idleAnimation.ResetAnimation();
-		idleAnimation.Play();
+		GetCurrentDashAnimation().Stop();
+		GetCurrentIdleAnimation().ResetAnimation();
+		GetCurrentIdleAnimation().Play();
 	}
 
 	void Start()
 	{
-		dashAnimation.OnLastFrameReached += OnFinishDashAnimation;
+		foreach (var dashAnimation in dashAnimations)
+		{
+			dashAnimation.OnLastFrameReached += OnFinishDashAnimation;
+		}
 	}
 }
