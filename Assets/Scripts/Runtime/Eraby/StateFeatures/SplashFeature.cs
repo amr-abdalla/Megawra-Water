cc90c8d76f3160ec3cb69c4ffec45346d220eb13
nyuriumuri <nyuriumuri@gmail.com>
2025-05-19 23:00:55 +0300

    pool splashes

--- Assets/Scripts/Runtime/Eraby/StateFeatures/SplashFeature.cs
+++ Assets/Scripts/Runtime/Eraby/StateFeatures/SplashFeature.cs
@@ -1,5 +1,5 @@
-
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SplashFeature : StateFeatureAbstract
 {
@@ -8,7 +8,6 @@ public class SplashFeature : StateFeatureAbstract
 
     [SerializeField]
     private Vector3 splashOffset = Vector3.zero;
-
 
     [SerializeField]
     private Quaternion splashRotation = Quaternion.identity;
@@ -22,11 +21,61 @@ public class SplashFeature : StateFeatureAbstract
     [SerializeField]
     private Vector3 puddleOffset = Vector3.zero;
 
+    // Instance pool for splash objects
+    private Queue<GameObject> splashPool = new Queue<GameObject>();
+    // Reserve pool for refilling splashPool
+    private Queue<GameObject> splashReservePool = new Queue<GameObject>();
+
+    // Instance pool for puddle objects
+    private Queue<GameObject> puddlePool = new Queue<GameObject>();
+    private Queue<GameObject> puddleReservePool = new Queue<GameObject>();
+
+    private Vector3 voidPosition = new Vector3(500, 500, 0);
+    protected override void Awake()
+    {
+        base.Awake();
+        if (splashPrefab != null)
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                GameObject obj = Instantiate(splashPrefab);
+                obj.transform.position = voidPosition;
+                splashReservePool.Enqueue(obj);
+            }
+        }
+        if (puddlePrefab != null)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                GameObject obj = Instantiate(puddlePrefab);
+                obj.transform.position = voidPosition;
+                puddleReservePool.Enqueue(obj);
+            }
+        }
+    }
 
     protected override void OnEnter()
     {
         if (splashPrefab == null) return;
-        GameObject splashObject = Instantiate(splashPrefab, transform.position + splashOffset, splashRotation);
+        GameObject splashObject = null;
+        if (splashPool.Count == 0 && splashReservePool.Count > 0)
+        {
+            // Refill splashPool from reserve
+            while (splashReservePool.Count > 0)
+            {
+                splashPool.Enqueue(splashReservePool.Dequeue());
+            }
+        }
+        if (splashPool.Count > 0)
+        {
+            splashObject = splashPool.Dequeue();
+            splashObject.transform.position = transform.position + splashOffset;
+            splashObject.transform.rotation = splashRotation;
+        }
+        else
+        {
+            splashObject = Instantiate(splashPrefab, transform.position + splashOffset, splashRotation);
+        }
 
         SpriteFrameSwapper splashFrameSwapper = splashObject.GetComponent<SpriteFrameSwapper>();
         if (splashFrameSwapper == null) return;
@@ -34,22 +83,40 @@ public class SplashFeature : StateFeatureAbstract
         splashFrameSwapper.StopLoop();
         splashFrameSwapper.OnLastFrameReached += () =>
         {
-            Destroy(splashObject);
+            splashReservePool.Enqueue(splashObject);
         };
         splashFrameSwapper.Play();
         if (spawnPuddle)
         {
-            GameObject puddleObject = Instantiate(puddlePrefab, transform.position, Quaternion.identity);
-            puddleObject.transform.SetParent(null);
+            GameObject puddleObject = null;
+            if (puddlePool.Count == 0 && puddleReservePool.Count > 0)
+            {
+                while (puddleReservePool.Count > 0)
+                {
+                    puddlePool.Enqueue(puddleReservePool.Dequeue());
+                }
+            }
+            if (puddlePool.Count > 0)
+            {
+                puddleObject = puddlePool.Dequeue();
+                puddleObject.transform.position = transform.position + puddleOffset;
+                puddleObject.transform.rotation = Quaternion.identity;
+                puddleObject.transform.SetParent(null);
+            }
+            else
+            {
+                puddleObject = Instantiate(puddlePrefab, transform.position + puddleOffset, Quaternion.identity);
+                puddleObject.transform.SetParent(null);
+            }
             SpriteFrameSwapper puddleFrameSwapper = puddleObject.GetComponent<SpriteFrameSwapper>();
             if (puddleFrameSwapper == null) return;
             puddleFrameSwapper.Stop();
             puddleFrameSwapper.StopLoop();
+            puddleFrameSwapper.OnLastFrameReached += () =>
+            {
+                puddleReservePool.Enqueue(puddleObject);
+            };
             puddleFrameSwapper.Play();
-
         }
     }
-
-
-
 }
